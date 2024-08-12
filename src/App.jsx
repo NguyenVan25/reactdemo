76ab2d4ad26805bf56849e68af3a44267a01c45c
nguyen thi thao van <nguyenvan25@MacBook-Air-cua-nguyen.local>
2024-08-12 22:18:04 +0700

    upload source code

--- src/App.jsx
+++ src/App.jsx
@@ -1,130 +1,133 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-// Đối tượng chứa link ảnh cho từng danh mục
-const images = {
-  Mountain: [
-    "https://savacotourist.com/upload/filemanager/NewFolder/Alps%20.jpg",
-    "https://savacotourist.com/upload/filemanager/NewFolder/nui-alpa-co-gi-dac-biet%20.jpg",
-    "https://savacotourist.com/upload/filemanager/NewFolder/thoi-tiet%20.jpg",
-    "https://savacotourist.com/upload/filemanager/NewFolder/day-nui-alps-di-qua-thuy-sy%20.jpg",
-    "https://savacotourist.com/upload/filemanager/NewFolder/alps-giap-nuoc-y%20.jpg",
-    "https://savacotourist.com/upload/filemanager/NewFolder/chamonix.jpg",
-    "https://savacotourist.com/upload/filemanager/NewFolder/Hallstatt-dia-qua-ao%20.jpg",
-    "https://www.tsttourist.com/vnt_upload/news/05_2022/tsttourist-diem-danh-5-ngon-nui-tuyet-dep-nhat-thuy-si-1.jpg",
-  ],
-  Beaches: [
-    "https://www.gbni.co.jp/content/img/2021/05/10.jpg",
-    "https://image.baogialai.com.vn/w800/dataimages/201711/original/images2596562_a_1.jpg",
-    "https://cdn.vntrip.vn/cam-nang/wp-content/uploads/2017/08/bien-li-son.jpg",
-    "https://cdn.vntrip.vn/cam-nang/wp-content/uploads/2017/08/canh-dep-dao-ly-son-21.jpg",
-    "https://topquangngai.vn/wp-content/uploads/2021/01/bai-bien-dep-o-Quang-Ngai-1.jpg",
-    "https://owa.bestprice.vn/images/destinations/uploads/bai-bien-an-hai-5f9b1750e316a.jpg",
-    "https://lalavieinvestment.com/uploads/images/7(11).png",
-    "https://lalavieinvestment.com/uploads/images/2(79).png",
-  ],
-  Birds: [
-    "https://camnangxnk-logistics.net/wp-content/uploads/2020/03/45e41f84c365692a3ddc01e8bf182db4.jpg",
-    "https://www.chimhay.net/wp-content/uploads/2023/06/chao-mao-lan.jpg",
-    "https://cdn.pixabay.com/photo/2022/10/24/14/41/blackbird-7543630_1280.jpg",
-    "https://cdn.pixabay.com/photo/2023/12/08/15/30/bird-8437764_1280.jpg",
-    "https://cdn.pixabay.com/photo/2023/12/08/15/30/bird-8437764_1280.jpg",
-    "https://cdn.pixabay.com/photo/2019/12/05/16/54/blackbird-4675637_1280.jpg",
-    "https://cdn.pixabay.com/photo/2018/04/25/17/48/bird-3350136_1280.jpg",
-    "https://cdn.pixabay.com/photo/2023/05/30/05/50/yellow-headed-blackbird-8027623_1280.jpg",
-  ],
-  Food: [
-    "https://cdn.pixabay.com/photo/2020/03/28/14/51/fried-chicken-4977349_1280.jpg",
-    "https://cdn.pixabay.com/photo/2017/08/27/03/25/cake-2685101_1280.jpg",
-    "https://cdn.pixabay.com/photo/2020/03/25/02/29/bread-4965907_1280.jpg",
-    "https://cdn.pixabay.com/photo/2020/03/25/02/31/rolls-4965914_1280.jpg",
-    "https://cdn.pixabay.com/photo/2024/05/15/13/30/food-8763576_1280.jpg",
-    "https://cdn.pixabay.com/photo/2023/05/27/12/35/noodles-8021415_1280.png",
-    "https://cdn.pixabay.com/photo/2021/10/21/12/48/beef-noodle-soup-6729018_1280.jpg",
-    "https://cdn.pixabay.com/photo/2021/10/21/12/38/pho-ga-6729006_1280.jpg",
-  ],
+const API_ENDPOINTS = {
+  Ethereum: "./tokenlist/Ethereum.json",
+  Arbitrum: "./tokenlist/Arbitrum.json",
+  Optimism: "./tokenlist/Optimism.json",
+  Bsc: "./tokenlist/Bsc.json",
+};
+
+// Thành phần Loading đơn giản
+const Loading = () => {
+  return <div className="loading"></div>;
 };
 
 function App() {
-  const [category, setCategory] = useState("Mountain");
+  const [category, setCategory] = useState("Ethereum");
+  const [tokens, setTokens] = useState([]);
+  const [isLoading, setLoading] = useState(true);
   const [searchTerm, setSearchTerm] = useState("");
-  const [filteredImages, setFilteredImages] = useState(images[category] || []);
-  const [message, setMessage] = useState("");
+  const [filteredTokens, setFilteredTokens] = useState([]);
+  const [showImages, setShowImages] = useState(false); // Thêm state để điều khiển hiển thị ảnh
 
-  const handleClick = (category) => {
-    setCategory(category);
-    setSearchTerm(""); // Reset search term when changing category
-    setFilteredImages(images[category]);
-    setMessage(""); // Reset message when changing category
+  useEffect(() => {
+    setLoading(true);
+    fetch(API_ENDPOINTS[category])
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setTokens(data);
+        setFilteredTokens(data);
+        setLoading(false);
+
+        // Bắt đầu timer hiển thị ảnh sau 4s
+        setTimeout(() => {
+          setShowImages(true);
+        }, 1000); // 4000 mili giây = 4 giây
+      })
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
+        setLoading(false);
+      });
+  }, [category]);
+
+  const handleClick = (selectedCategory) => {
+    setCategory(selectedCategory);
+    setSearchTerm("");
+    setShowImages(false); // Reset showImages khi thay đổi category
   };
 
   const handleSearch = () => {
-    if (searchTerm && images[searchTerm]) {
-      setFilteredImages(images[searchTerm]);
-      setMessage(""); // Reset message if search term is found
-    } else {
-      setFilteredImages([]);
-      setMessage("No found");
-    }
+    setFilteredTokens(
+      tokens.filter((token) =>
+        token.symbol.toLowerCase().startsWith(searchTerm.toLowerCase())
+      )
+    );
+    setShowImages(false); // Reset showImages khi tìm kiếm
   };
+  const DEFAULT_TOKEN_IMAGE =
+    "https://cdn.imgbin.com/9/8/16/imgbin-computer-icons-question-mark-scalable-graphics-blue-question-mark-icon-white-question-mark-n3SxnveXUmn5aQ5jsUSiPZ48T.jpg";
 
   return (
-    <>
-      <div className="container">
-        <h1 id="one">SnapShot</h1>
-        <div className="search-container">
-          <input
-            type="text"
-            className="search"
-            placeholder="Search"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          <button type="button" className="submit" onClick={handleSearch}>
-            <svg height="30" width="30" viewBox="0 0 64 64">
-              <circle
-                cx="27"
-                cy="27"
-                r="16"
-                stroke="white"
-                strokeWidth="4"
-                fill="none"
-              />
-              <line
-                x1="41"
-                y1="41"
-                x2="58"
-                y2="58"
-                stroke="white"
-                strokeWidth="4"
-              />
-            </svg>
-          </button>
-        </div>
-        <div className="button">
-          <button onClick={() => handleClick("Mountain")}>Mountain</button>
-          <button onClick={() => handleClick("Beaches")}>Beaches</button>
-          <button onClick={() => handleClick("Birds")}>Birds</button>
-          <button onClick={() => handleClick("Food")}>Food</button>
-        </div>
-        <div>
-          <h2>
-            {filteredImages.length > 0 ? `${category} Pictures` : message}
-          </h2>
-          <div className="image-gallery">
-            {filteredImages.length > 0
-              ? filteredImages.map((image, index) => (
+    <div className="container">
+      <h1 id="one">SnapShot</h1>
+      <div className="search-container">
+        <input
+          type="text"
+          className="search"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button type="button" className="submit" onClick={handleSearch}>
+          <svg height="30" width="30" viewBox="0 0 64 64">
+            <circle
+              cx="27"
+              cy="27"
+              r="16"
+              stroke="white"
+              strokeWidth="4"
+              fill="none"
+            />
+            <line
+              x1="41"
+              y1="41"
+              x2="58"
+              y2="58"
+              stroke="white"
+              strokeWidth="4"
+            />
+          </svg>
+        </button>
+      </div>
+      <div className="button">
+        <button onClick={() => handleClick("Ethereum")}>Ethereum</button>
+        <button onClick={() => handleClick("Arbitrum")}>Arbitrum</button>
+        <button onClick={() => handleClick("Optimism")}>Optimism</button>
+        <button onClick={() => handleClick("Bsc")}>Bsc</button>
+      </div>
+      <div>
+        <h2>
+          {filteredTokens.length > 0 ? `${category} Tokens` : "No tokens found"}
+        </h2>
+        <div className="token-gallery">
+          {isLoading
+            ? Array.from({ length: 20 }).map((_, index) => (
+                <Loading key={index} />
+              ))
+            : showImages
+            ? // Hiển thị tối đa 20 token đầu tiên
+              filteredTokens.slice(0, 20).map((token, index) => (
+                <div key={index} className="token-item">
                   <img
-                    key={index}
-                    src={image}
-                    alt={`${category} ${index}`}
+                    src={token.logoURI ? token.logoURI : DEFAULT_TOKEN_IMAGE}
                     className="gallery-img"
+                    onError={(e) => (e.target.src = DEFAULT_TOKEN_IMAGE)} // Xử lý lỗi khi tải ảnh
+                    title={token.symbol}
                   />
-                ))
-              : null}
-          </div>
+                </div>
+              ))
+            : // Hiển thị loading nếu showImages là false
+              Array.from({ length: 20 }).map((_, index) => (
+                <Loading key={index} />
+              ))}
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
